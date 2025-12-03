abd96d949ed9d6819d0359bb2270c78b6cfb9fd4
Md Mahmudur Rahman <mdmahmudurrahman648@gmail.com>
2025-12-03 13:25:06 +0600

    doing the seller profile #5

--- src/pages/seller/Profile.jsx
+++ src/pages/seller/Profile.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react';
-import { Edit2 } from 'lucide-react';
+
+import { User, Edit , ChevronLeft } from 'lucide-react';
 
 export default function SellerProfile() {
  
 
   return (
   <div className='min-h-screen bg-gray-50 p-4 sm:p-6 lg:p-8'>
-
+        <div className='max-w-7xl mx-auto bg-white rounded-lg shadow-sm p-6'>
+         {/*header */} 
+<div className='flex items-center gap-2 mb-8'>
+<button className='flex items-center text-gray-700 hover:text-green-500'>
+  <ChevronLeft size={22} strokeWidth={2.5}  />
+<span className='text-lg'>Seller Profile</span>
+</button>
+</div>
+        </div>
   </div>
   );
 }
